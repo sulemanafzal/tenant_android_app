db599293032fd283d9415d0ad2e5d567f47c8ba4
SulemanAfzal <sulemanafzalbkuc1998@gmail.com>
2024-06-07 17:25:47 +0500

    signup page design

---
+++ app/src/main/java/com/example/tenant/activity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.tenant.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.tenant.R
+import com.example.tenant.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private var isBackPressed = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding?.root)
+
+        //hide Status bar
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+       //ADD navigation graph
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+}
